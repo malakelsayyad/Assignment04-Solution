5c91e89ebf17bbe9f6d4505b0bb828370f497256
malakelsayyad <malakelsayyad@yahoo.com>
2025-01-18 19:42:22 +0200

    Assignment

---
+++ Assignment/DurationClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class DurationClass
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+        public DurationClass(int hours , int minutes,int seconds )
+        { 
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;  
+        }
+
+        public DurationClass(int totalSeconds)
+        {
+            Hours = totalSeconds / 3600;
+            Minutes = (totalSeconds % 3600) / 60; 
+            Seconds = totalSeconds % 60; 
+        }
+        public override string ToString()
+        {
+            string result = "";
+            if (Hours > 0)
+                result += $"Hours: {Hours}, ";
+            if (Minutes > 0 || Hours > 0) 
+                result += $"Minutes: {Minutes}, ";
+            result += $"Seconds: {Seconds}";
+            return result;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is DurationClass duration)
+            {
+                return Hours == duration.Hours && Minutes == duration.Minutes && Seconds == duration.Seconds;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Hours, Minutes, Seconds);
+        }
+    }
+}
---
+++ Assignment/SecondProject/MathClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.SecondProject
+{
+    public static class MathClass
+    {
+        public static double Add(double num1, double num2)
+        {
+            return num1+num2;
+        }
+
+        public static double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+
+        }
+
+        public static double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+             
+            }
+            return num1 / num2;
+        }
+    }
+}
